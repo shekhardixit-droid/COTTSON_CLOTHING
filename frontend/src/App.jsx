7d2c1bf4e29d2767506f4a2155ada28d51d7a18a
aditya25666 <adityaihp25@gmail.com>
2026-09-26 09:22:53 +0530

    app.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
-
-import Home from "./pages/Home";
 import Clients from "./pages/Clients";
 
 function App() {
